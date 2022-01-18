f970d1a3c1ee97fb14606c9ac39651075cdb45ea
Nozimjon <nozimjon1806@gmail.com>
2022-01-18 11:34:03 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,6 @@
 <?php
 $_SESSION['page'] = "Воридшавӣ";
 include("pages/login.php");
-// if ($_SESSION['page']==="Бақайдгирӣ"){
-// 	header("location: pages/register.php");
-// die;
-// }
-// else{
-// 	header("location: pages/login.php");
-// }
-	
-
 
 
 ?>
